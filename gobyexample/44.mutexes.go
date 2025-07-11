c33e5018853702dd4eb29192c87d074e85ac181b
cherry77 <15071285862@163.com>
2025-07-11 10:55:34 +0800

    feat(gobyexample): Mutexes

---
+++ gobyexample/44.mutexes.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Container struct {
+	mu       sync.Mutex
+	counters map[string]int
+}
+
+func (c *Container) inc(name string) {
+	c.mu.Lock()
+	c.counters[name]++
+	c.mu.Unlock()
+}
+func main() {
+	c := Container{
+		counters: map[string]int{"a": 0, "b": 0},
+	}
+
+	var wg sync.WaitGroup
+
+	doIncrement := func(name string, n int) {
+		for range n {
+			c.inc(name)
+		}
+		wg.Done()
+	}
+	wg.Add(3)
+	go doIncrement("a", 1000)
+	go doIncrement("a", 1000)
+	go doIncrement("b", 1000)
+
+	wg.Wait()
+	fmt.Println("counters:", c.counters)
+}
